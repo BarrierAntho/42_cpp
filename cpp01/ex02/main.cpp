b571edf7ad7c50395a984791e684da74828a9ad1
BarrierAntho <barrier.antho@gmail.com>
2022-10-18 21:06:10 +0200

    ADD - cpp01-ex02 completed but need to add more details cout
    BODY:
    N/A
    
    TODO:
    N/A
    
    	nouveau fichier : ex02/Makefile
    	nouveau fichier : ex02/hi_this_is_brain
    	nouveau fichier : ex02/main.cpp

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarrier <abarrier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 18:28:19 by abarrier          #+#    #+#             */
+/*   Updated: 2022/10/18 21:05:20 by abarrier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF= str;
+
+	std::cout << static_cast<void*>(&str) << std::endl;
+	std::cout << static_cast<void*>(stringPTR) << std::endl;
+	std::cout << static_cast<void*>(&stringREF) << std::endl;
+
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return (0);
+}
