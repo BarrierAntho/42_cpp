ca7ff7a268216027b1ec671e4794b7be30d4ab81
BarrierAntho <barrier.antho@gmail.com>
2022-10-18 15:59:46 +0200

    UPDT - change constructor with init of value
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         Zombie.cpp

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -6,21 +6,19 @@
 /*   By: abarrier <abarrier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 14:24:39 by abarrier          #+#    #+#             */
-/*   Updated: 2022/10/18 15:06:10 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/10/18 15:58:33 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(void)
+Zombie::Zombie(void): name(ZB_DFT_NAME)
 {
-	this->name = ZB_DFT_NAME;
 	announce();
 }
 
-Zombie::Zombie(std::string name)
+Zombie::Zombie(std::string name): name(name)
 {
-	this->name = name;
 	announce();
 }
 
