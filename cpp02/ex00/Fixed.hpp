e766ade436e6581af4b20043ceb2103f2b85d3cd
BarrierAntho <barrier.antho@gmail.com>
2022-10-30 20:12:49 +0100

    ADD - init cpp02
    BODY:
    N/A
    
    TODO:
    N/A
    
    	nouveau fichier : cpp02/42_cpp02.pdf
    	nouveau fichier : cpp02/ex00/Fixed.cpp
    	nouveau fichier : cpp02/ex00/Fixed.hpp
    	nouveau fichier : cpp02/ex00/Makefile
    	nouveau fichier : cpp02/ex00/main.cpp

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarrier <abarrier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 17:00:43 by abarrier          #+#    #+#             */
+/*   Updated: 2022/10/24 21:12:03 by abarrier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed(void);
+
+	private:
+		int			value;
+		static const int	frac_bits = 8;
+};
+
+#endif
