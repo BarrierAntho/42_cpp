bb8cd614dbc01ba9f974de1247f91c66216ef5ac
Anthony BARRIER <abarrier@e3r7p21.clusters.42paris.fr>
2022-11-17 10:12:22 +0100

    UPDT - cpp05-ex00 test completed
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   main.cpp

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:11:03 by abarrier          #+#    #+#             */
-/*   Updated: 2022/11/16 14:21:04 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/11/17 10:08:11 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,145 @@
 
 #define SEP_P "####################"
 #define SEP_SP "--------------------"
+#define SEP_SSP "...................."
 
 int	main( void )
 {
 	std::cout << "Program - Start" << std::endl;
-	try
+	std::cout << SEP_P << std::endl;
 	{
-		Bureaucrat	b1("Zeus", 1);
-		Bureaucrat	b2("Ares", 150);
-//		Bureaucrat	*b3 = new Bureaucrat("Poseidon", 1);
-		Bureaucrat	b11("test", 0);
-//		Bureaucrat	b12("test", 151);
-//		delete(b3);
+		// TEST - CONSTRUCTOR / DESTRUCTOR / OVERLOAD OPERATOR
+		std::cout << "TEST - CONSTRUCTOR / DESTRUCTOR / OVERLOAD OPERATOR" << std::endl;
+		std::cout << SEP_SP << std::endl;
+		try
+		{
+			std::cout << "CONSTRUCTOR - DEFAULT" << std::endl;
+			std::cout << "b0" << std::endl;
+			Bureaucrat	b0;
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "CONSTRUCTOR - WITH ARGUMENT" << std::endl;
+			std::cout << "b1(\"Zeus\", 150)" << std::endl;
+			Bureaucrat	b1("Zeus", 150);
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "CONSTRUCTOR - COPY" << std::endl;
+			std::cout << "b2(b1)" << std::endl;
+			Bureaucrat	b2(b1);
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "OVERLOAD OPERATOR" << std::endl;
+			std::cout << "b3 = b0" << std::endl;
+			Bureaucrat	b3 = b0;
+			std::cout << "b0 = b1" << std::endl;
+			b0 = b1;
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "SHOW" << std::endl;
+			std::cout << "bo: " << b0 << std::endl;
+			std::cout << "b1: " << b1 << std::endl;
+			std::cout << "b2: " << b2 << std::endl;
+			std::cout << "b3: " << b3 << std::endl;
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "DESTRUCTOR" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
-	catch (std::exception &e)
+	std::cout << SEP_P << std::endl;
 	{
-		std::cerr << e.what() << std::endl;;
+		// TEST - GETTER
+		std::cout << "TEST - GETTER" << std::endl;
+		std::cout << SEP_SP << std::endl;
+		try
+		{
+			std::cout << "CONSTRUCTOR - DEFAULT" << std::endl;
+			std::cout << "b0" << std::endl;
+			Bureaucrat	b0;
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "GETTER" << std::endl;
+			std::cout << "b0 Name: " << b0.getName() << std::endl;
+			std::cout << "b0 Grade: " << b0.getGrade() << std::endl;
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "SHOW" << std::endl;
+			std::cout << "bo: " << b0 << std::endl;
+			std::cout << SEP_SP << std::endl;
+
+			std::cout << "DESTRUCTOR" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
+	std::cout << SEP_P << std::endl;
+	{
+		// TEST - ERROR
+		// CREATE GRADE < 1
+		std::cout << "TEST - ERROR" << std::endl;
+		std::cout << SEP_SP << std::endl;
+		std::cout << "CREATE GRADE < 1" << std::endl;
+		try
+		{
+			std::cout << "b1(\"Zeus\", 0)" << std::endl;
+			Bureaucrat	b1("Zeus", 0);
+			std::cout << "If this line appears, error is not catch" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;;
+		}
+		std::cout << SEP_SP << std::endl;
+
+		// CREATE GRADE > 150
+		std::cout << "CREATE GRADE > 150" << std::endl;
+		try
+		{
+			std::cout << "b1(\"Zeus\", 151)" << std::endl;
+			Bureaucrat	b1("Zeus", 151);
+			std::cout << "If this line appears, error is not catch" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;;
+		}
+		std::cout << SEP_SP << std::endl;
+
+		// CREATE GRADE = 1 THEN INCREMENT
+		std::cout << "CREATE GRADE = 1 THEN INCREMENT" << std::endl;
+		try
+		{
+			std::cout << "b1(\"Zeus\", 1)" << std::endl;
+			Bureaucrat	b1("Zeus", 1);
+			b1.incrementGrade();
+			std::cout << "If this line appears, error is not catch" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;;
+		}
+		std::cout << SEP_SP << std::endl;
+
+		// CREATE GRADE = 151 THEN DECREMENT
+		std::cout << "CREATE GRADE = 151 THEN DECREMENT" << std::endl;
+		try
+		{
+			std::cout << "b1(\"Zeus\", 150)" << std::endl;
+			Bureaucrat	b1("Zeus", 150);
+			b1.decrementGrade();
+			std::cout << "If this line appears, error is not catch" << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;;
+		}
+	}
+	std::cout << SEP_P << std::endl;
 	std::cout << "Program - End" << std::endl;
 	return (0);
 }
